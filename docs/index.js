42e5e76efe5ad70fc0dd038e578c1ff322abe2b9
Damian <damiansuaz@gmail.com>
2025-04-08 13:22:13 -0400

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,36 @@
+const menuEle = document.querySelector("nav").querySelector("menu");
+
+const navList = [
+  {
+    place: "Home",
+    tip: "The root webpage",
+    link: "#"
+  },
+  {
+    place: "Projects",
+    tip: "All completed builds",
+    link: "#"
+  },
+  {
+    place: "Blog",
+    tip: "Written progress updates",
+    link: "#"
+  }
+];
+
+window.onload = function() {
+  if(menuEle && navList) {
+    navList.forEach((n) => {
+      const insertNav = document.createElement("a");
+      const liEle = document.createElemtent("li");
+      const tooltip = document.createElement("span");
+      tooltip.classList.add('tooltip');
+      tooltip.innerHTML = n.tip;
+      liEle.innerHTML = n.place;
+      insertNav.href = n.link;
+      insertNav.appendChild(liEle);
+      insertNav.appendChild(tooltip);
+      navList.appendChild(insertNav);
+    });
+  }
+};
